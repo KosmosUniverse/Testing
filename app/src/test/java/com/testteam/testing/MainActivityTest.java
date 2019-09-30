17b6978d07debc11fae31c4696d8b0b2462ecee5
KosmosUniverse <ducret.thomas@gmail.com>
2019-09-30 01:22:55 +0100

    Initial commit

---
+++ app/src/test/java/com/testteam/testing/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.testteam.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+    }
+}
